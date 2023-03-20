5d718b6c654d0c694e5ac4d7f776286147fdb977
Gilbert <gilberthuarcaya@gmail.com>
2023-03-20 00:26:10 -0500

    Initial Commit

---
+++ my_first_app/ContentView.swift
@@ -0,0 +1,29 @@
+//
+//  ContentView.swift
+//  my_first_app
+//
+//  Created by Gilbert Alonso Huarcaya Matias on 3/19/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+            Button(/*@START_MENU_TOKEN@*/"Button"/*@END_MENU_TOKEN@*/) {
+                /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+            }
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ my_first_app/my_first_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  my_first_appApp.swift
+//  my_first_app
+//
+//  Created by Gilbert Alonso Huarcaya Matias on 3/19/23.
+//
+
+import SwiftUI
+
+@main
+struct my_first_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
